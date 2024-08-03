3509b562ab2e0ee7e88d1067cf9ec893a112573b
Blessing <ogorblessing96@gmail.com>
2024-08-03 01:34:54 +0000

    Tasks 3 to 4 done

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,12 @@
 
 from flask import Flask, render_template
 from models import storage
-from app_views import api.v1.views
+from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_err(error):
@@ -19,7 +21,7 @@ if __name_ == "__main__":
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
     if not host:
-	host = '0.0.0.0.0'
+        host = '0.0.0.0'
     if not port:
-	port = '5000'
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,5 +4,16 @@ from api.v1.views import app_views
 
 @app_views.route(/status, method=['Get']strict_slashes=False)
 def status():
-	"""Returns the api status"""
-	return jsonify({"status": "OK"})
+    """Returns the api status"""
+    return jsonify({"status": "OK"})
+
+@app_views.routr(/stats, strict=False)
+def count():
+    """Counts the number of each object by type"""
+    classes = [Amenity, City, Places, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    
+    obj_num = {}
+    for i in classes:
+    obj_num[names[i]] = storage.count(classes[i])
+    return jsonifyobj_num)

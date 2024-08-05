14906f5cb93ae1510d3deb53f9a501c0a9cc88a9
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-08-05 14:40:24 +0000

    tested task 3 4 5 and it works

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
-from api.v1.views.index import *
+#!/usr/bin/python3
 
-app_views = Blueprint('__init__', __name__, url_prefix = '/api/v1')
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__,  url_prefix = '/api/v1')
+
+
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,23 @@
-from api.v1.views import app_views
-from flask import jsonify
+#!/usr/bin/python3
 
-@app_views.route('/status')
+from flask import jsonify
+from models import storage
+from api.v1.views import app_views
+from models.engine import file_storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    error_msg = {"status": "OK"}
-    return jsonify(error_msg)
+    msg = {"status": "OK"}
+    return jsonify(msg)
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    dic = {}
+    for cls in file_storage.classes.keys():
+        dic[cls] = storage.count(cls)
+    return jsonify(dic)
+
+
+if __name__ == "__main__":
+    pass

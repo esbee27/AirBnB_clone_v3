7599e63d6a5779fe47e00087e326aea8c26ab8f6
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-08-06 16:57:54 +0000

    corrected circular import error in init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,8 +4,9 @@ This module initializes the Blueprint for the API views.
 """
 
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *

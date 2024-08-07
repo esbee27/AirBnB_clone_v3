2bd40e4ac462cb57268390ddcad364b0b21d7c2e
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-08-07 01:48:34 +0000

    added users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,85 +1,57 @@
 #!/usr/bin/python3
 """
-This module defines routes for handling User objects via a RESTful API.
+    Flask route that returns json response
 """
-
-from flask import jsonify, abort, request
-from models.user import User
-from models import storage
 from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models import storage, CNC
 
 
-@app_views.route('/users', methods=['GET', 'POST'], strict_slashes=False)
-def handle_users():
+@app_views.route('/users/', methods=['GET', 'POST'])
+def users_no_id(user_id=None):
     """
-    Retrieves all User objects or creates a new User object.
-
-    - GET: Returns a list of all User objects.
-    - POST: Creates a new User object with the provided data.
-
-    Returns:
-        - GET: JSON list of User objects.
-        - POST: JSON representation of the newly created User object.
+        users route that handles http requests with no ID given
     """
-    if request.method == 'GET':
-        objects = storage.all(User)
-        obj_list = [value.to_dict() for value in objects.values()]
-        return jsonify(obj_list)
-    elif request.method == 'POST':
-        try:
-            content = request.get_json()
-        except ValueError:
-            return jsonify({'error': 'Not a JSON'}), 400
-        if not isinstance(content, dict):
-            return jsonify({'error': 'Not a JSON'}), 400
-        if 'email' not in content:
-            return jsonify({'error': 'Missing email'}), 400
-        if 'password' not in content:
-            return jsonify({'error': 'Missing password'}), 400
-
-        new_user = User(**content)
-        storage.new(new_user)
-        storage.save()
-        return jsonify(new_user.to_dict()), 201
-
-
-@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'],
-                 strict_slashes=False)
-def handle_user(user_id):
-    """
-    Retrieves, deletes, or updates a User object by its ID.
-
-    - GET: Retrieves a User object by its ID.
-    - DELETE: Deletes a User object by its ID.
-    - PUT: Updates a User object with the provided data.
-
-    Args:
-        user_id (str): The ID of the User object.
-
-    Returns:
-        - GET: JSON representation of the User object.
-        - DELETE: Empty JSON object with status code 200.
-        - PUT: JSON representation of the updated User object.
-    """
-    user = storage.get(User, user_id)
-    if user is None:
-        abort(404)
 
     if request.method == 'GET':
-        return jsonify(user.to_dict())
-    elif request.method == 'DELETE':
-        storage.delete(user)
-        storage.save()
+        all_users = storage.all('User')
+        all_users = [obj.to_json() for obj in all_users.values()]
+        return jsonify(all_users)
+
+    if request.method == 'POST':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        if req_json.get('email') is None:
+            abort(400, 'Missing email')
+        if req_json.get('password') is None:
+            abort(400, 'Missing password')
+        User = CNC.get('User')
+        new_object = User(**req_json)
+        new_object.save()
+        return jsonify(new_object.to_json()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
+def user_with_id(user_id=None):
+    """
+        users route that handles http requests with ID given
+    """
+    user_obj = storage.get('User', user_id)
+    if user_obj is None:
+        abort(404, 'Not found')
+
+    if request.method == 'GET':
+        return jsonify(user_obj.to_json())
+
+    if request.method == 'DELETE':
+        user_obj.delete()
+        del user_obj
         return jsonify({}), 200
-    elif request.method == 'PUT':
-        try:
-            content = request.get_json()
-        except ValueError:
-            return jsonify({'error': 'Not a JSON'}), 400
-        if not isinstance(content, dict):
-            return jsonify({'error': 'Not a JSON'}), 400
-        for key, value in content.items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(user, key, value)
-        storage.save()
-        return jsonify(user.to_dict()), 200
+
+    if request.method == 'PUT':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        user_obj.bm_update(req_json)
+        return jsonify(user_obj.to_json()), 200
